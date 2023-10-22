614ddb810bbd76ef8cd976f9158beefc4dfac578
Allan Mercado <mapes911@gmail.com>
2023-10-21 18:14:16 -0700

    fixes to get first uploads working

--- src/midstream.ts
+++ src/midstream.ts
@@ -1,4 +1,11 @@
-import { Video } from "./types/video";
+import { v4 as uuidv4 } from "uuid";
+
+import {
+  PresignedUploadDetails,
+  PaginatedVideoList,
+  UploadVideoResponse,
+  Video,
+} from "./types/video";
 
 // const API_URL: string = "https://api.production.midstream.cc";
 const API_URL: string = "http://localhost:7039";
@@ -18,7 +25,7 @@ export class Midstream {
     this.apiKey = apiKey;
   }
 
-  protected async doGet<T>(url: string) {
+  protected async doGet<T>(url: string): Promise<T> {
     const response = await fetch(`${API_URL}${url}`, {
       method: "GET",
       headers: {
@@ -29,8 +36,57 @@ export class Midstream {
     return await response.json();
   }
 
-  getVideos(page: number = 1) {
-    return this.doGet<Video[]>(`/api/orgs/${this.orgId}/videos?page=${page}`);
+  protected async doPost<T>(url: string, data: { [key: string]: string }) {
+    const response = await fetch(`${API_URL}${url}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "X-API-Key": this.apiKey,
+      },
+    });
+    if (!response.ok) throw new Error(response.statusText);
+    return await response.json();
   }
-  // uploadVideo()
+
+  protected async doGetPresignedUploadUrl(key: string) {
+    return this.doPost<PresignedUploadDetails>(
+      `/api/orgs/${this.orgId}/videos/presigned`,
+      { key },
+    );
+  }
+
+  async getVideos(page: number = 1): Promise<PaginatedVideoList> {
+    return this.doGet<PaginatedVideoList>(
+      `/api/orgs/${this.orgId}/videos?page=${page}`,
+    );
+  }
+
+  async uploadVideo(file: File, name: string): Promise<UploadVideoResponse> {
+    if (!file.type.includes("video")) {
+      throw new Error("only video types are supported");
+    }
+    const newFileKey = uuidv4();
+    const uploadUrlDetails = await this.doGetPresignedUploadUrl(newFileKey);
+
+    // upload directly to S3
+    const formData = new FormData();
+    for (const field in uploadUrlDetails.fields) {
+      formData.append(field, uploadUrlDetails.fields[field]);
+    }
+    formData.append("file", file);
+    const uploadResponse = await fetch(uploadUrlDetails.url, {
+      method: "POST",
+      body: formData,
+    });
+    if (!uploadResponse.ok) throw new Error(uploadResponse.statusText);
+
+    // add the video to the org
+    const addResponse = await this.doPost(`/api/orgs/${this.orgId}/videos`, {
+      key: uploadUrlDetails.fields["key"],
+      name,
+    });
+    return {
+      id: addResponse.id,
+    };
+  }
 }
--- src/types/video.ts
+++ src/types/video.ts
@@ -1,9 +1,21 @@
-// export type VideoFiles = {};
-
 export type Video = {
   id: string;
   name: string;
   thumbnail_file: string;
   encoded_at: Date;
   // encoded_files: VideoFiles;
+};
+
+export type PaginatedVideoList = {
+  count: number;
+  items: Video[];
+};
+
+export type PresignedUploadDetails = {
+  url: string;
+  fields: { [key: string]: string };
+};
+
+export type UploadVideoResponse = {
+  id: string;
 };
